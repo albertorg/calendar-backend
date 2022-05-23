10d053dc328688afb567ca9ca61c4d4e61719ca9
Alberto Rodriguez <ticoc911@gmail.com>
2022-05-23 21:15:30 +0200

    end server

--- controllers/events.js
+++ controllers/events.js
@@ -3,7 +3,7 @@ const Events = require('../models/events')
 
 
 
-const getEvents = async (req, res = response, next) => {
+const getEvents = async (req, res = response) => {
 
     const events = await Events.find()
         .populate('user', 'name')
@@ -14,7 +14,7 @@ const getEvents = async (req, res = response, next) => {
     })
 }
 
-const createEvent = async (req, res = response, next) => {
+const createEvent = async (req, res = response) => {
 
     const event = new Events(req.body)
 
@@ -37,20 +37,86 @@ const createEvent = async (req, res = response, next) => {
     }
 }
 
-const updateEvent = (req, res = response, next) => {
+const updateEvent = async(req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'updateEvents'
-    })
+    const eventId = req.params.id
+    const userId = req.uid
+ 
+    try {
+        const event = await Events.findById(eventId)
+
+        if (!event) {
+            res.status(404).json({
+                ok: false,
+                msg: 'No event exists with tis ID'
+            })
+        }
+
+        if (event.user.toString() !== userId) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'You do not have permission for this action'
+            })
+        }
+
+        const newEvent = {
+            ...req.body,
+            user: userId
+        }
+
+        const updatedEvent = await Events.findByIdAndUpdate(eventId, newEvent, {new: true})
+
+        res.json({
+            ok: true,
+            evento: updatedEvent
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact with admin'
+        })
+    }
+
 }
 
-const deleteEvent = (req, res = response, next) => {
+const deleteEvent = async(req, res = response) => {
 
-    res.json({
-        ok: true,
-        msg: 'deleteEvents'
-    })
+    const eventId = req.params.id
+    const userId = req.uid
+
+    try {
+        const event = await Events.findById(eventId)
+
+        if (!event) {
+            res.status(404).json({
+                ok: false,
+                msg: 'No event exists with tis ID'
+            })
+        }
+
+        if (event.user.toString() !== userId) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'You do not have permission for this action'
+            })
+        }
+
+        const deletedEvent = await Events.findByIdAndDelete(eventId)
+
+        res.json({
+            ok: true,
+            evento: deletedEvent
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Please contact with admin'
+        })
+    }
 }
 
 
